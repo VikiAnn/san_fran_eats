0233e6223c5d4156fc35b0ac3595ee4a0332d37a
Viki Harrod <victoria.harrod@gmail.com>
2023-11-08 14:45:09 -0700

    Use prettier

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-import express, { Request, Response } from 'express';
-import favicon from 'serve-favicon';
-import path from 'path';
+import express, { Request, Response } from "express";
+import favicon from "serve-favicon";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send("This will be SanFranEats, serving up data sourced from the DataSF Open Data collection");
+app.get("/", (req: Request, res: Response) => {
+  res.send(
+    "This will be SanFranEats, serving up data sourced from the DataSF Open Data collection"
+  );
 });
 
-app.use(favicon(path.join(__dirname, 'images', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, "images", "favicon.ico")));
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
