ae35cfa5cdaebac690a74487f4966a6c793df93c
Viki Harrod <victoria.harrod@gmail.com>
2023-11-08 14:42:11 -0700

    Initial commit: working express.js endpoint, favicon, and eslint

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import favicon from 'serve-favicon';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("This will be SanFranEats, serving up data sourced from the DataSF Open Data collection");
+});
+
+app.use(favicon(path.join(__dirname, 'images', 'favicon.ico')))
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
